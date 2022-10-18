e6618dd8493f44382716e25f7566c6d8197189c0
WouterNexclic <wouter@nexclic.com>
2022-10-18 14:27:22 +0200

    added privacy page

---
+++ Privacy.php
@@ -0,0 +1,179 @@
+<div data-v-cba8e71e="" data-v-aa6eca8e="" class="container" data-v-6809c568="">
+    <div data-v-aa6eca8e="" class="row mt-3">
+        <div data-v-aa6eca8e="" class="col-sm-12">
+            <div data-v-aa6eca8e="" class="card review-body-card p-5"><p><strong>Please Note - Using our site and
+                        services will indicate that you have read, as well as accepted, this Privacy Policy. We ask that
+                        you read our Privacy Policy carefully and that you use our services if, and only if, you agree
+                        to provisions of this privacy policy.</strong><br>
+                    &nbsp;</p>
+
+                <p>This Privacy Policy is effective as of the date stated above and supersedes all earlier versions of
+                    our Privacy Policy.<img height="19" src="https://panel.nexclic.com/storage/images/9868054436082d05decc900-93254029.png" width="74">(the “Company"), at its sole discretion, reserves the right to
+                    make changes to this Privacy Policy at any time. Your continued use of the website constitutes your
+                    ongoing consent to this Privacy Policy, and to any changes to it. Please note that the most current
+                    version is reflected by the "last revised" heading and we advise you to check it periodically.<br>
+                    The Company takes its customers’ privacy seriously. The following document details our privacy
+                    practices and explains what personal information, if at all, we collect, how we use it, who do we
+                    share it with and your rights with regards to the information. The Company takes&nbsp;many measures
+                    to ensure that your information remains as secure and confidential as possible.<br>
+                    &nbsp;<br>
+                    <strong>What information do we collect?</strong><br>
+                    The Company collects the following information:<br>
+                    When you access our website, we may collect information from your device, such as IP address.<br>
+                    When you register and/or use our services we collect your personal information. For example, when
+                    you sign up in order to write a review, we will collect your email address. This will be saved to
+                    our database and used to identify you on your next login. When you provide your contact information
+                    in connection with a third party’s&nbsp;&nbsp;product or service, which may include your first and
+                    last name, e-mail address and phone number, we will save it to our database and transfer it to such
+                    third party (who may be abroad), who will process, use and store it in accordance with its own
+                    privacy policy.<br>
+                    We may also collect and store non-personal information (information which is non-identifiable) about
+                    how you use the website including pages viewed and where you were linked to us from.<br>
+                    We will never ask you for personal financial data such as bank account numbers or payment
+                    details.<br>
+                    &nbsp;<br>
+                    <strong>How do we use the information we collect?</strong><br>
+                    The Company may use your personal information for the following purposes:<br>
+                    To enable you to access and use our services;<br>
+                    To communicate with you when you use the "Contact Us" feature on our website;<br>
+                    To communicate with you via notifications and updates, including sending you information about
+                    products and services which we think may be of interest to you;<br>
+                    To make internal statistics and data analysis, so we can improve our services.<br>
+                    &nbsp;<br>
+                    <strong>How do we share this information?</strong><br>
+                    Part of using our services means you consent to us disclosing some of your personal information, if
+                    necessary, to third parties.<br>
+                    Such third parties may be the providers of certain services or products presented on our Site. In
+                    such case, we will transfer to such third party the contact information that you fill in in the
+                    respective form in connection with such service or product. This third-party service provider will
+                    be responsible for the processing of the information.&nbsp;<br>
+                    In addition, such third parties may include: analytics tools and cloud host providers.<br>
+                    Additionally, as our services are hosted by a third-party provider, Amazon Web Services (AWS), some
+                    of your information may be stored with Amazon's S3 storage service. You can find more information on
+                    Amazon's data security from the&nbsp;S3 website.<br>
+                    Please note that some data recipients may be located outside the EEA. In such cases we will transfer
+                    your information only to such countries approved by the European Commission as providing adequate
+                    level of data protection, or enter into legal agreement ensuring an adequate level of data
+                    protection.<br>
+                    &nbsp;<br>
+                    <strong>Do we use cookies?</strong><br>
+                    To enhance your browsing experience, we do use web cookies. We delete most of our cookies as soon as
+                    you close your browser, while some are stored until deleted or expired.&nbsp;<br>
+                    We use Google Analytics, a web analytics service provided by Google, Inc. ('Google'). Google
+                    Analytics uses 'cookies',&nbsp;text files placed on your computer, to help the web team analyze how
+                    users use the site. The information generated by the cookie (including your IP address) is
+                    transmitted to and stored by Google on servers in the United States. Google uses this information
+                    for the purpose of evaluating your use of the website, compiling reports on website activity for
+                    website operators and providing other services relating to website activity and internet usage.
+                    Google may also transfer this information to third parties where required to do so by law, or where
+                    such third parties process the information on Google's behalf. Google will not associate your IP
+                    address with any other data held by Google. You can prevent Google Analytics from recognizing you on
+                    return visits by&nbsp;disabling cookies&nbsp;on your browser. For more information on Google
+                    Analytics see:&nbsp;https://www.google.com/analytics/standard/features/. For more information on
+                    opting-out of Google Analytics see:&nbsp;https://support.google.com/analytics/answer/181881?hl=en&amp;ref_topic=2919631<br>
+                    You may refuse the use of cookies by selecting the appropriate settings on your browser, however,
+                    please note that by doing so, you may not be able to use the full functionality of this website.<br>
+                    The Company and third-party vendors, including Google, use first-party cookies (such as the Google
+                    Analytics cookie) and third-party cookies (such as the DoubleClick cookie) together to inform,
+                    optimize, and serve ads based on past website visits. By using our services, you consent to us using
+                    cookies in the manner and for the purposes set out above. The&nbsp;Google Analytics Opt-out Browser
+                    Add-on&nbsp;can be used to opt out and customize these advertisements.<br>
+                    We may also use the Facebook pixel, which can be used to serve you ads or interact with you on
+                    social media based on your browsing behavior.<br>
+                    &nbsp;<br>
+                    <strong>User rights</strong><br>
+                    You may request to:<br>
+                    Receive confirmation as to whether or not personal information concerning you is being processed and
+                    access your stored personal information, together with supplementary information.<br>
+                    Receive a copy of personal information you directly provided to us in a structured, commonly used
+                    and machine-readable format.<br>
+                    Request rectification of your personal information that is in our control.<br>
+                    Request the erasure of your personal information<br>
+                    Object to the processing of your personal information by us.<br>
+                    Request to restrict processing of your personal information by us.<br>
+                    Lodge a complaint with a supervisory authority.<br>
+                    However, please note that these rights are not absolute, and may be subjected to our own legitimate
+                    interest and regulatory requirements.<br>
+                    &nbsp;If you wish to exercise any of the aforementioned rights, or receive more information, please
+                    contact us at<img height="21" src="https://panel.nexclic.com/storage/images/8092047046082d0344645c3-56376067.png" width="122">.<br>
+                    If you would like to make any requests or queries regarding personal data that we transfer to or
+                    process on the third-party service provider’s behalf, please contact them directly. For example, if
+                    you wish to access, correct, or delete data processed by us on behalf of the third-party service
+                    provider, please direct your request to the relevant third-party service provider (who is the “data
+                    controller” of such data). Should we receive such requests directly, we may refer them to the
+                    third-party service provider.<br>
+                    &nbsp;<br>
+                    <strong>Retention Period</strong><br>
+                    We will retain your personal data for as long as necessary in order to provide our service. If you
+                    wish that we stop processing your data, we will retain and use your private data as long as required
+                    in order to comply with our legal obligations, resolve disputes and enforce our policies.&nbsp;<br>
+                    When you provide your content information in connection with a third party’s product or service, we
+                    disclose such personal information to the third-party service provider shortly after you submit your
+                    details on the Site. After that, we will retain your personal data for up to 6 months or for such
+                    period as instructed by the third-party service provider who controls such data.&nbsp;<br>
+                    &nbsp;<br>
+                    <strong>How do we safeguard and transfer your information</strong><br>
+                    We take great care in implementing and maintaining the security of this site and your information.
+                    Our website uses SSL certificate and encryption, and we employ industry standard procedures and
+                    policies to ensure the safety of our users' information, and prevent unauthorized use of such
+                    information. Please keep in mind that data transmission over the internet can never be fully secure.
+                    There is always some risk involved in uploading your information online, and any information you
+                    upload or input is done so at your own risk.<br>
+                    As for third parties, we require any third party to comply with similar security measures, in
+                    accordance with this Privacy Policy. However, we cannot be responsible for the acts of those who
+                    gain unauthorized access or abuse our services, and we make no warranty, express, implied or
+                    otherwise, that we will prevent such access.<br>
+                    &nbsp;<br>
+                    <strong>Third party collection of information</strong><br>
+                    Our Privacy Policy only addresses the use and disclosure of information we collect from you. To the
+                    extent that you disclose your information to other parties via our site (e.g. by clicking on a link
+                    to any other website or location, or fill in contact information in a specific form) or via other
+                    sites throughout the internet, different rules may apply to their use or disclosure of the
+                    information you disclose to them.<br>
+                    It is important to note that such third party services may have their own privacy policies and we
+                    advise you to read them carefully. Inclusion of a link to third party website or service is not a
+                    recommendation of such a website or service. Third party may store and use the information in
+                    different jurisdictions, who may not impose the same level of protection as your own jurisdiction.
+                    Third party sites may contain information or service that is illegal, unreasonable or that some
+                    people may find inappropriate or offensive. Before using third party sites and services, providing
+                    third parties personal information or making a purchase, we recommend you to read and understand
+                    those sites’, service’ or product’s terms and conditions, warranty, and privacy policies and to
+                    ensure you agree to their terms. You acknowledge that we are not responsible for the products,
+                    services, or descriptions of those products or services that you receive from third party sites or
+                    to the content or privacy practices of those sites, and that this Privacy Policy does not apply to
+                    any such third party products and services. You are knowingly and voluntarily assuming all risks of
+                    using third party sites to purchase products and services and providing your personal information.
+                    You agree that we shall have no liability whatsoever with respect to such third party sites and your
+                    usage of them.<br>
+                    &nbsp;<br>
+                    <strong>Corporate Transaction</strong><br>
+                    We may share or transfer your information (including personal information) if we enter into a
+                    business transaction such as a merger, acquisition, reorganization, bankruptcy, or sale of some or
+                    all of our assets. Any party that acquires our assets as part of such a transaction may continue to
+                    use your information in accordance with the terms of this Privacy Policy.<br>
+                    &nbsp;<br>
+                    <strong>Minors</strong><br>
+                    Our site and services are not designated to users under the age of 18. If you are under 18, you
+                    should not use the site nor provide any personal information to us.<br>
+                    &nbsp;We reserve the right to access and verify any personal information collected from you. In the
+                    event that we become aware that a user under the age of 18 has shared any information, we will
+                    discard such information. If you have any reason to believe that a minor has shared any information
+                    with us, please contact us at&nbsp;<img height="21" src="https://panel.nexclic.com/storage/images/7948713426082d00dc6da90-25989581.png" width="122"><br>
+                    &nbsp;<br>
+                    <strong>How can you help us improve?</strong><br>
+                    In an attempt to ensure that the services we provide continue to fit all of your needs, we will
+                    periodically ask you for feedback. Feel free to tell us what’s on your mind and we'll try to improve
+                    accordingly. We will not publish any of your feedback without your written consent.<br>
+                    &nbsp;<br>
+                    <strong>When will we contact you?</strong><br>
+                    By using our services, you agree that we are entitled to send you information via email. Information
+                    may include products, services and offers we think may interest you.<br>
+                    &nbsp;That said, customers will always be able to opt-out of receiving these updates. If you do
+                    decide to unsubscribe, you can always do so by contacting&nbsp;<img height="24" src="https://panel.nexclic.com/storage/images/2072419806082d08b1c8469-16739953.png" width="150"><br>
+                    &nbsp;<br>
+                    Contact information:<br>
+                    &nbsp;<img height="48" src="https://panel.nexclic.com/storage/images/10734777366082cffd18e897-60558704.png" width="150"></p>
+            </div>
+        </div>
+    </div>
+</div>
